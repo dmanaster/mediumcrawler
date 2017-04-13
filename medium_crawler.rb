540280b30e7e2c117926b78ee5e56884b0655297
dmanaster <david@manaster.com>
2017-04-13 14:46:01 -0400

    Now identifies Medium sites by their meta information instead of URI.

--- medium_crawler.rb
+++ medium_crawler.rb
@@ -10,37 +10,33 @@ def crawl_site( starting_at, &each_page )
   counter = 0
 
   crawl_page = ->(page_uri) do              # A re-usable mini-function
-    name = get_name(page_uri)
-    unless seen_pages.include?(name)
-      seen_pages << name                # Record that we've seen this
+    unless seen_pages.include?(page_uri)
+      seen_pages << page_uri                # Record that we've seen this
       begin
-        counter = counter+1
-        puts counter.to_s + " " + page_uri.to_s
         doc = Nokogiri.HTML(open(page_uri)) # Get the page
+        
+        if doc.xpath("//meta[@name='twitter:app:name:iphone']/@content").first.to_s == "Medium"
+          counter = counter+1
+          puts counter.to_s
 
-        each_page.call(doc,page_uri)        # Yield page and URI to the block
+          each_page.call(doc,page_uri)        # Yield page and URI to the block
 
-        # Find all the links on the page
-        selector = "//a[starts-with(@href, \"https://\")]/@href"
+          # Find all the links on the page
+          selector = "//a[starts-with(@href, \"https://\")]/@href"
+          hrefs = doc.xpath selector
 
-        hrefs = doc.xpath selector
+          # Make these URIs, throwing out problem ones like mailto:
+          uris = hrefs.map{ |href| URI.join( page_uri, href ) rescue nil }.compact
 
-        # Make these URIs, throwing out problem ones like mailto:
-        uris = hrefs.map{ |href| URI.join( page_uri, href ) rescue nil }.compact
+          # Throw out links to files (this could be more efficient with regex)
+          uris.reject!{ |uri| files.any?{ |ext| uri.path.end_with?(".#{ext}") } }
 
-        # Reject producthunt.com URLs, which have a similar syntax
-        uris.reject!{ |uri| uri.host == "www.producthunt.com" }
+          # Remove #foo fragments so that sub-page links aren't differentiated
+          uris.each{ |uri| uri.fragment = nil }
 
-        uris.select!{ |uri| uri.path.start_with? ("/@")  }
-
-        # Throw out links to files (this could be more efficient with regex)
-        uris.reject!{ |uri| files.any?{ |ext| uri.path.end_with?(".#{ext}") } }
-
-        # Remove #foo fragments so that sub-page links aren't differentiated
-        uris.each{ |uri| uri.fragment = nil }
-
-        # Recursively crawl the child URIs
-        uris.each{ |uri| crawl_page.call(uri) }
+          # Recursively crawl the child URIs
+          uris.each{ |uri| crawl_page.call(uri) }
+        end
 
       rescue OpenURI::HTTPError # Guard against 404s
         warn "Skipping invalid link #{page_uri}"
@@ -54,15 +50,11 @@ end
 def search_page(page_uri)
   page = Nokogiri.HTML(open(page_uri))
   if page.css('p.hero-description').to_s.downcase.include?("developer")
-    puts "!!!!!!" + " " + page_uri.to_s + " " + "!!!!!!"
+    puts page_uri.to_s
   end
 end
 
-def get_name(uri)
-  uri.path[1..-1].partition("/").first
-end
-
-crawl_site('https://medium.com/google-developers') do |page,uri|
+crawl_site('https://medium.com/@retomeier') do |page,uri|
   # page here is a Nokogiri HTML document
   # uri is a URI instance with the address of the page
   search_page(uri)
