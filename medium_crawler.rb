e708c8ac1f44734ae2197b65997371421c7431df
dmanaster <david@manaster.com>
2017-04-13 12:42:07 -0400

    Introduces quick fixes for issues around domain names not being on medium.com

--- medium_crawler.rb
+++ medium_crawler.rb
@@ -7,22 +7,29 @@ def crawl_site( starting_at, &each_page )
   files = %w[png jpeg jpg gif svg txt js css zip gz]
   starting_uri = URI.parse(starting_at)
   seen_pages = Set.new                      # Keep track of what we've seen
+  counter = 0
 
   crawl_page = ->(page_uri) do              # A re-usable mini-function
-    unless seen_pages.include?(page_uri)
-      seen_pages << page_uri                # Record that we've seen this
+    name = get_name(page_uri)
+    unless seen_pages.include?(name)
+      seen_pages << name                # Record that we've seen this
       begin
+        counter = counter+1
+        puts counter.to_s + " " + page_uri.to_s
         doc = Nokogiri.HTML(open(page_uri)) # Get the page
+
         each_page.call(doc,page_uri)        # Yield page and URI to the block
 
         # Find all the links on the page
-        hrefs = doc.css('a[href]').map{ |a| a['href'] }
+        selector = "//a[starts-with(@href, \"https://\")]/@href"
+
+        hrefs = doc.xpath selector
 
         # Make these URIs, throwing out problem ones like mailto:
         uris = hrefs.map{ |href| URI.join( page_uri, href ) rescue nil }.compact
 
-        # Pare it down to only those pages that are on the same site
-        uris.select!{ |uri| uri.host == starting_uri.host }
+        # Reject producthunt.com URLs, which have a similar syntax
+        uris.reject!{ |uri| uri.host == "www.producthunt.com" }
 
         uris.select!{ |uri| uri.path.start_with? ("/@")  }
 
@@ -46,11 +53,16 @@ end
 
 def search_page(page_uri)
   page = Nokogiri.HTML(open(page_uri))
-  puts page_uri.path
-  puts page.css('p.hero-description')
+  if page.css('p.hero-description').to_s.downcase.include?("developer")
+    puts "!!!!!!" + " " + page_uri.to_s + " " + "!!!!!!"
+  end
 end
 
-crawl_site('https://medium.com/') do |page,uri|
+def get_name(uri)
+  uri.path[1..-1].partition("/").first
+end
+
+crawl_site('https://medium.com/google-developers') do |page,uri|
   # page here is a Nokogiri HTML document
   # uri is a URI instance with the address of the page
   search_page(uri)
